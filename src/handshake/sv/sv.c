ce9ef0bd3191108951856dfddd6311a8cf5e8f17
Techflash <72118300+techflashYT@users.noreply.github.com>
2023-05-23 17:26:39 -0700

    update server handshake to match last commit

--- src/handshake/sv/sv.c
+++ src/handshake/sv/sv.c
@@ -7,6 +7,8 @@ void handshakeSv() {
 	printf("Running as %sserver%s.\r\n", B_CYAN, RESET);
 	// wait for a CL_HSTART
 	rxCL_HSTART();
-	// SV_HSTART
+	// send a SV_HSTART
 	txSV_HSTART();
+	// wait for a CL_TFMMWVer
+	rxCL_TFMMWVer();
 }
